98a2edf91245aac52521480369de1e4eb9cabefc
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2022-08-26 11:29:36 +0200

    fix

--- core/compat/psp/syspatch.c
+++ core/compat/psp/syspatch.c
@@ -374,7 +374,7 @@ int StartModuleHandler(int modid, SceSize argsize, void * argp, int * modstatus,
 
     SceModule2* mod = (SceModule2*) sceKernelFindModuleByUID(modid);
 
-    if(skip_logos && mod != NULL && 0 == strcmp(mod->modname, "vsh_module")) {
+    if (skip_logos && mod != NULL && ark_config->launcher[0] == 0 && 0 == strcmp(mod->modname, "vsh_module") ) {
 		u32* vshmain_args = oe_malloc(1024);
 
 		memset(vshmain_args, 0, 1024);
