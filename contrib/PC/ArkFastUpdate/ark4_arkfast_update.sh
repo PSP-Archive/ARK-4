04cefc841e050293e240da9aaa08cfa7ac772a5f
Yoti <realyoti@gmail.com>
2022-09-23 23:19:52 +0300

    Update ark4_arkfast_update.sh

--- contrib/PC/ArkFastUpdate/ark4_arkfast_update.sh
+++ contrib/PC/ArkFastUpdate/ark4_arkfast_update.sh
@@ -7,6 +7,9 @@
 #sudo apt update
 #sudo apt install zip unzip -y
 
+if [ -f "ArkFast.vpk" ]; then
+    rm -f ArkFast.vpk
+fi
 wget -q --show-progress "https://github.com/ONElua/ArkFast/releases/latest/download/ArkFast.vpk"
 if [ -f "ArkFast.vpk" ]; then
     unzip ArkFast.vpk -d ArkFast_tmp
@@ -16,6 +19,9 @@ else
     exit
 fi
 
+if [ -f "ARK4.zip" ]; then
+    rm -f ARK4.zip
+fi
 wget -q --show-progress "https://github.com/PSP-Archive/ARK-4/releases/latest/download/ARK4.zip"
 if [ -f "ARK4.zip" ]; then
     unzip ARK4.zip -d ARK4_tmp
@@ -28,13 +34,12 @@ fi
 if [ -f "ArkFast_new.vpk" ]; then
     rm -f ArkFast_new.vpk
 fi
-
 cd ARK4_tmp/
 mv -f VitaBubble/K.BIN ARK_01234/K.BIN
 zip -r ARK_01234.zip ARK_01234
 mv -f ARK_01234.zip ../ArkFast_tmp/resources/ARK_01234.zip
 cd ..
-rm -rf ARK4_tmp
+rm -rf ARK4_tmp/
 cd ArkFast_tmp/
 zip -r ../ArkFast_new.vpk .
 cd ..
