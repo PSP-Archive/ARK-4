b66d4abdd4e83c864de9a6391697c459345b2813
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-01-15 12:29:43 +0100

    it compiles? SHIP IT!

--- core/compat/pentazemin/syspatch.c
+++ core/compat/pentazemin/syspatch.c
@@ -11,6 +11,17 @@
 #include "exitgame.h"
 #include "libs/graphics/graphics.h"
 
+typedef struct {
+    char *name;
+    void *buf;
+    int size;
+    int unk_12;
+    int attr;
+    int unk_20;
+    int argSize;
+    int argPartId;
+} SceLoadCoreBootModuleInfo;
+
 extern STMOD_HANDLER previous;
 
 // Return Boot Status
@@ -56,6 +67,73 @@ int (*_sceAudioOutput2GetRestSample)();
 int sceAudioOutput2ReleaseFixed(){
     if (_sceAudioOutput2GetRestSample() > 0) return -1;
 	return _sceAudioOutput2Release();
+}
+
+SceUID sceKernelLoadModuleBufferBootInitBtcnfPatched(SceLoadCoreBootModuleInfo *info, void *buf, int flags, SceKernelLMOption *option) {
+
+	char path[64];
+	sprintf(path, "ms0:/__ADRENALINE__/flash0%s", info->name); //not use flash0 cause of cxmb
+
+	SceUID mod = sceKernelLoadModule661(path, 0, NULL);
+	if (mod >= 0)
+		return mod;
+
+	return sceKernelLoadModuleBufferBootInitBtcnf661(info->size, buf, flags, option);
+}
+
+SceUID (* LoadModuleBufferAnchorInBtcnf)(void *buf, int a1);
+SceUID LoadModuleBufferAnchorInBtcnfPatched(void *buf, SceLoadCoreBootModuleInfo *info) {
+	char path[64];
+	sprintf(path, "ms0:/__ADRENALINE__/flash0%s", info->name);
+
+	SceUID mod = sceKernelLoadModule661(path, 0, NULL);
+	if (mod >= 0)
+		return mod;
+
+	return LoadModuleBufferAnchorInBtcnf(buf, (info->attr >> 8) & 1);
+}
+
+int (*ARKPatchInit)(int (* module_bootstart)(SceSize, void *), void *argp) = NULL;
+int AdrenalinePatchInit(int (* module_bootstart)(SceSize, void *), void *argp) {
+	u32 init_addr = ((u32)module_bootstart) - 0x1A54;
+
+	// Ignore StopInit
+	_sw(0, init_addr + 0x18EC);
+
+	// Redirect load functions to load from MS
+	LoadModuleBufferAnchorInBtcnf = (void *)init_addr + 0x1038;
+	MAKE_CALL(init_addr + 0x17E4, LoadModuleBufferAnchorInBtcnfPatched);
+	_sw(0x02402821, init_addr + 0x17E8); //move $a1, $s2
+
+	_sw(0x02402021, init_addr + 0x1868); //move $a0, $s2
+	MAKE_CALL(init_addr + 0x1878, sceKernelLoadModuleBufferBootInitBtcnfPatched);
+
+	flushCache();
+
+	return ARKPatchInit(module_bootstart, argp);
+}
+
+// Patch Loader Core Module
+SceModule2* patchLoaderCore(void)
+{
+    // Find Module
+    SceModule2* mod = (SceModule2 *)sceKernelFindModuleByName("sceLoaderCore");
+
+    // Fetch Text Address
+    u32 start_addr = mod->text_addr;
+    u32 topaddr = mod->text_addr+mod->text_size;
+
+    int found = 0;
+    for (u32 addr = start_addr; addr<topaddr&&!found; addr+=4){
+        u32 data = _lw(addr);
+        if (data == 0x02E02021){
+            ARKPatchInit = K_EXTRACT_CALL(addr-4);
+            _sw(JAL(AdrenalinePatchInit), addr-4);
+            found = 1;
+        }
+    }
+
+    return mod;
 }
 
 void AdrenalineOnModuleStart(SceModule2 * mod){
@@ -121,5 +199,6 @@ exit:
 }
 
 void AdrenalineSysPatch(){
-    SceModule2* mod = NULL;
+    SceModule2* loadcore = patchLoaderCore();
+
 }
