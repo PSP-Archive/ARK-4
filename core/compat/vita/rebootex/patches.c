af2b712891447d5a98bcfc6839ea9f750523d7b2
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-02-09 19:13:43 +0100

    some fixes on rebootex

--- core/compat/vita/rebootex/patches.c
+++ core/compat/vita/rebootex/patches.c
@@ -18,7 +18,8 @@ int _pspemuLfatOpen(BootFile* file, int unk)
     char* p = file->name;
     if (strcmp(p, "pspbtcnf.bin") == 0){
         p[2] = 'v'; // custom btcnf for PS Vita
-        if (reboot_conf->iso_mode == MODE_INFERNO){
+        if (reboot_conf->iso_mode == MODE_INFERNO || reboot_conf->iso_mode == MODE_MARCH33 || reboot_conf->iso_mode == MODE_NP9660){
+            reboot_conf->iso_mode = MODE_INFERNO;
             p[5] = 'i'; // use inferno ISO mode (psvbtinf.bin)
         }
         // else use psvbtcnf.bin for np9660
