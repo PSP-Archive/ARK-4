1f2fde28719786e27b18db977376a2e61f119cbb
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-02-25 14:18:35 +0100

    bumped to 2GB

--- core/stargate/io_patch.c
+++ core/stargate/io_patch.c
@@ -30,7 +30,7 @@ static u32 _sceIoDevctl(const char *name, int cmd, u32 argAddr, int argLen, u32 
         u32 sectorSize = deviceSize->sectorSize;
         u32 memStickSectorSize = 32 * 1024;
         u32 sectorCount = memStickSectorSize / sectorSize;
-        u32 freeSize = 1 * 1024 * 1024 * 1024;
+        u32 freeSize = 2 * 1024 * 1024 * 1024;
         u32 maxClusters = (u32)(freeSize / (sectorSize * sectorCount));
 
         if (deviceSize->freeClusters > maxClusters){
