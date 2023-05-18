df312cf296f0ee7aeec0f37e7d09ee9b28e46d3b
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-05-18 14:07:09 +0200

    added option to restart and shutdown in custom launcher

--- extras/menus/arkMenu/include/exit_mgr.h
+++ extras/menus/arkMenu/include/exit_mgr.h
@@ -2,21 +2,60 @@
 #define EXIT_MGR_H
 
 #include "system_entry.h"
+#include "optionsmenu.h"
 
 extern "C"{
     void sctrlKernelExitVSH(void*);
+    void scePowerRequestColdReset(int);
+    void scePowerRequestStandby();
 }
+
+static t_options_entry exit_opts[] = {
+    {0, "Exit"},
+    {1, "Restart"},
+    {2, "Shutdown"},
+};
 
 class ExitManager : public SystemEntry{
 
+    /* Options Menu instance, will be drawn by the draw thread if it's different from null */
+    OptionsMenu* optionsmenu;
+
     public:
-        void draw(){};
-        void control(Controller* pad){};
+        void draw(){
+            if (optionsmenu) optionsmenu->draw();
+        };
+        void control(Controller* pad){
+            if (optionsmenu){
+                int ret = optionsmenu->control();
+                switch (ret){
+                    case 0:
+                        sctrlSESetUmdFile("");
+          	            sctrlSESetBootConfFileIndex(MODE_UMD);
+                        sctrlKernelExitVSH(NULL);
+                        break;
+                    case 1:
+                        scePowerRequestColdReset(0);
+                        break;
+                    case 2:
+                        scePowerRequestStandby();
+                        break;
+                }
+                OptionsMenu* aux = optionsmenu;
+                optionsmenu = NULL;
+                delete aux;
+            }
+        };
         void pause(){};
         void resume(){
-            sctrlSESetUmdFile("");
-          	sctrlSESetBootConfFileIndex(MODE_UMD);
-            sctrlKernelExitVSH(NULL);
+            if (IS_PSP(common::getArkConfig())){
+                optionsmenu = new OptionsMenu("", sizeof(exit_opts)/sizeof(t_options_entry), exit_opts);
+            }
+            else {
+                sctrlSESetUmdFile("");
+          	    sctrlSESetBootConfFileIndex(MODE_UMD);
+                sctrlKernelExitVSH(NULL);
+            }
         };
         std::string getInfo(){return "Exit";};
         void setInfo(std::string info){};
