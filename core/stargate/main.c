7a4b2e4ed5ec293220078721d73c7f16d8fab0c0
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-06-05 12:19:32 +0200

    patch ATV Offroad Fury PRO

--- core/stargate/main.c
+++ core/stargate/main.c
@@ -42,8 +42,15 @@ void stargateSyspatchModuleOnStart(SceModule2 * mod)
 {
 
     if (strcmp(mod->modname, "tekken") == 0) {
-        hookImportByNID(mod, "scePower", 0x34F9C463, 222);
+        hookImportByNID(mod, "scePower", 0x34F9C463, 222); // scePowerGetPllClockFrequencyInt
 	}
+
+    else if (strcmp(mod->modname, "ATVPRO") == 0){
+        hookImportByNID(mod, "scePower", 0x843FBF43, 0);   // scePowerSetCpuClockFrequency
+        hookImportByNID(mod, "scePower", 0xFDB5BFE9, 222); // scePowerGetCpuClockFrequencyInt
+        hookImportByNID(mod, "scePower", 0xBD681969, 111); // scePowerGetBusClockFrequencyInt
+    }
+    
     else if (strcasecmp(mod->modname, "DJMAX") == 0) {
         hookImportByNID(mod, "IoFileMgrForUser", 0xE3EB004C, 0);
     }
