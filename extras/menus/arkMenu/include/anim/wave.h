4037206f472b216656a2f5ac33e44ffc5a4cef7b
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-06-12 21:08:35 +0200

    better wave colors

--- extras/menus/arkMenu/include/anim/wave.h
+++ extras/menus/arkMenu/include/anim/wave.h
@@ -11,8 +11,8 @@
 #define WAVES_FRAMESKIP 0 // anything higher and the waves will not move smoothly, lower and the fps drop
 #define WAVE_AMPLITUDE 30 // how much the wave moves on the screen
 // these colors allow the background's color to standout, and they look great
-#define WAVE_COLOR 0x44E0E0E0
-#define WAVE_COLOR2 0x44C0C0C0
+#define WAVE_COLOR 0xa0cccc00
+#define WAVE_COLOR2 0xa0808000
 #define WAVE_COLOR_OUTLINE 0x44A0A0A0
 
 typedef struct
