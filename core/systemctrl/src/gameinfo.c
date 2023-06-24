311c4b36a4eb45ea6265dec2c5633948fbfccf99
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-06-24 13:34:37 +0200

    cleanup and fix per-game settings/plugins

--- core/systemctrl/src/gameinfo.c
+++ core/systemctrl/src/gameinfo.c
@@ -29,7 +29,7 @@ int readGameIdFromDisc(char* gameid){
         param.lba_top = 16;
         param.byte_size_total = 10;
         param.byte_size_start = 883;
-
+        
         int res = sceIoDevctl("umd:", 0x01E380C0, &param, sizeof(param), game_id, sizeof(game_id));
 
         if (res < 0) return 0;
@@ -43,49 +43,27 @@ int readGameIdFromDisc(char* gameid){
         game_id[9] = 0;
     }
 
-    if (gameid) strcpy(gameid, game_id);
+    if (gameid) memcpy(gameid, game_id, 9);
     return 1;
 }
 
-/*
-int readGameIdFromDisc(char* gameid){
-    // Open Disc Identifier
-    int disc = sceIoOpen("disc0:/UMD_DATA.BIN", PSP_O_RDONLY, 0777);
-    // Opened Disc Identifier
-    if(disc >= 0)
-    {
-        // Read Country Code
-        sceIoRead(disc, gameid, 4);
-        
-        // Skip Delimiter
-        sceIoLseek32(disc, 1, PSP_SEEK_CUR);
-        
-        // Read Game ID
-        sceIoRead(disc, gameid + 0x4, 5);
-        
-        // Close Disc Identifier
-        sceIoClose(disc);
-        return 1;
-    }
-    return 0;
-}
-*/
-
 int getGameId(char* gameid){
-    if (!readGameIdFromDisc(gameid)){
-        // Find Function
-        void * (* SysMemForKernel_EF29061C)(void) = (void *)sctrlHENFindFunction("sceSystemMemoryManager", "SysMemForKernel", 0xEF29061C);
-        
-        // Function unavailable (how?!)
-        if(SysMemForKernel_EF29061C == NULL) return 0;
-        
-        // Get Game Info Structure
-        void * gameinfo = SysMemForKernel_EF29061C();
-        
-        // Structure unavailable
-        if(gameinfo == NULL) return 0;
-        
-        memcpy(gameid, gameinfo+0x44, 9);
-    }
+    // Find Function
+    void * (* SysMemForKernel_EF29061C)(void) = (void *)sctrlHENFindFunction("sceSystemMemoryManager", "SysMemForKernel", 0xEF29061C);
+    
+    // Function unavailable (how?!)
+    if(SysMemForKernel_EF29061C == NULL) return 0;
+    
+    // Get Game Info Structure
+    void * gameinfo = SysMemForKernel_EF29061C();
+    
+    // Structure unavailable
+    if(gameinfo == NULL) return 0;
+    
+    memcpy(gameid, gameinfo+0x44, 9);
+
+    if (gameid[0] == 0 || strncmp(gameid, "HOME00000", 9) == 0)
+        return readGameIdFromDisc(gameid);
+
     return 1;
 }
