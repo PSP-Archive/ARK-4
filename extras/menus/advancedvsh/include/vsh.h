fec5aa6304e82e67297fdb74574d120d2d7e13c4
pyroesp <nicolaslopezesp@gmail.com>
2023-06-25 14:11:34 +0200

    add vsh_Menu struct
    used to move all (most) global variables to a single structure used everywhere

---
+++ extras/menus/advancedvsh/include/vsh.h
@@ -0,0 +1,58 @@
+#ifndef _VSH_H
+#define _VSH_H
+
+
+#include <psptypes.h>
+#include <pspctrl.h>
+
+#include "common.h"
+
+
+// Config stuff go here
+typedef struct _vsh_Config{
+	// SE config
+	SEConfig se;
+	SEConfig old_se;
+	
+	// ARK menu config
+	t_conf ark_menu;
+	t_conf old_ark_menu;
+	
+	// ARK config
+	ARKConfig ark;
+	ARKConfig *p_ark;
+}vsh_Config;
+
+// Button stuff go here
+typedef struct{
+	SceCtrlData pad;
+	u32 new_buttons_on;
+}vsh_Buttons;
+
+// Status stuff go here
+typedef struct _vsh_Status{
+	u32 swap_xo;
+	
+	int menu_mode;
+	int submenu_mode;
+	int stop_flag;
+	int sub_stop_flag;
+	
+	int reset_vsh;
+}vsh_Status;
+
+// VSH Menu struct
+typedef struct _vsh_Menu{
+	SceUID thread_id;
+	
+	vsh_Config config;
+	vsh_Buttons buttons;
+	vsh_Status status;
+	
+	u32 psp_model;
+	
+	int battery;
+}vsh_Menu;
+
+
+#endif
