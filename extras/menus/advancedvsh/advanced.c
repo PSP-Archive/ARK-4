22d0805c0abb67008946c094094ec6b66f661fc7
pyroesp <nicolaslopezesp@gmail.com>
2023-06-25 14:37:58 +0200

    make header + use vsh_Menu struct + use scePaf

--- extras/menus/advancedvsh/advanced.c
+++ extras/menus/advancedvsh/advanced.c
@@ -2,72 +2,55 @@
 #include <psputility.h>
 #include <time.h>
 
+#include "systemctrl.h"
+#include "clock.h"
+
+#include "vsh.h"
+#include "font.h"
+#include "advanced.h"
+
 extern int pwidth;
 extern char umd_path[72];
-extern SEConfig cnf;
 
 extern char device_buf[13];
 extern char umdvideo_path[256];
 
-extern xyPoint[];
-extern xyPoint2[];
+extern int xyPoint[];
+extern int xyPoint2[];
 
-extern ARKConfig* ark_config;
-extern int cur_battery;
-extern t_conf config;
-extern u32 swap_xo;
 
-enum {
-	SUBMENU_USB_DEVICE,
-	SUBMENU_USB_READONLY,
-	SUBMENU_UMD_MODE,
-	SUBMENU_UMD_VIDEO,
-	SUBMENU_FG_COLORS,
-	SUBMENU_BG_COLORS,
-	SUBMENU_FONT,
-	SUBMENU_CONVERT_BATTERY,
-	SUBMENU_SWAP_XO_BUTTONS,
-	SUBMENU_REGION_MODE,
-	SUBMENU_UMD_REGION_MODE,
-	SUBMENU_IMPORT_CLASSIC_PLUGINS,
-	SUBMENU_ACTIVATE_FLASH_WMA,
-	SUBMENU_DELETE_HIBERNATION,
-	SUBMENU_RANDOM_GAME,
-	SUBMENU_GO_BACK,
-	SUBMENU_MAX
-};
+extern vsh_Menu *g_vsh_menu;
+int sub_stop_stock = 0;
+
 
 int item_fcolor[SUBMENU_MAX];
 const char *subitem_str[SUBMENU_MAX];
-
 static int submenu_sel = SUBMENU_USB_DEVICE;
 
-extern char* available_fonts[];
 
-int submenu_draw(void)
-{
-	const char msg[128] = {0};
+int submenu_draw(void) {
+	char msg[128] = {0};
 	int submax_menu, subcur_menu;
 	const int *pointer;
 	u32 fc, bc;
 
 	// ARK Version
-	const char ark_version[24];
+	char ark_version[24];
 	int ver = sctrlHENGetMinorVersion();
  	int major = (ver & 0xFF0000) >> 16;
 	int minor = (ver & 0xFF00) >> 8;
 	int micro = (ver & 0xFF);
 
 	#ifdef DEBUG
-    if (micro > 0) 
-		scePaf_snprintf(ark_version, sizeof(ark_version), " ARK %d.%d.%.2i DEBUG ", major, minor, micro);
-    else 
-		scePaf_snprintf(ark_version, sizeof(ark_version), " ARK %d.%d DEBUG ", major, minor);
-    #else
-    if (micro > 0) 
+	if (micro > 0) 
+		scePaf_snprintf(ark_version, sizeof(ark_version), "    ARK %d.%d.%.2i DEBUG    ", major, minor, micro);
+	else 
+		scePaf_snprintf(ark_version, sizeof(ark_version), "    ARK %d.%d DEBUG    ", major, minor);
+	#else
+	if (micro > 0) 
 		scePaf_snprintf(ark_version, sizeof(ark_version), "    ARK %d.%d.%.2i    ", major, minor, micro);
-    else 
-		scePaf_snprintf(ark_version, sizeof(ark_version), "      ARK %d.%d     ", major, minor); 
+	else 
+		scePaf_snprintf(ark_version, sizeof(ark_version), "    ARK %d.%d    ", major, minor); 
 	#endif
 
 	// check & setup video mode
@@ -88,7 +71,7 @@ int submenu_draw(void)
 	
 	for (submax_menu = 0; submax_menu < SUBMENU_MAX; submax_menu++) {
 		// do bg_color stuff
-		switch(config.vsh_bg_color) {
+		switch(g_vsh_menu->config.ark_menu.vsh_bg_color) {
 			// Random
 			case 0:
 			// Red
@@ -237,7 +220,7 @@ int submenu_draw(void)
 		}
 
 		// do fg_color stuff
-		switch(config.vsh_fg_color) {
+		switch(g_vsh_menu->config.ark_menu.vsh_fg_color) {
 			// Random
 			case 0:
 			// White  
@@ -400,7 +383,9 @@ int submenu_draw(void)
 				int subitem_start_x = (pointer[6] * 8) + 128;
 				if(subitem_str[submax_menu]) {
 					// check if PSP Go or PSVita because UMD Region mode is unsupported on them
-					if ((psp_model == PSP_GO || IS_VITA_ADR(ark_config)) && submax_menu == SUBMENU_UMD_REGION_MODE){
+					if ((g_vsh_menu->psp_model == PSP_GO || 
+						IS_VITA_ADR(g_vsh_menu->config.p_ark)) && 
+						submax_menu == SUBMENU_UMD_REGION_MODE) {
 						// write the unsupported string
 						scePaf_snprintf(msg, 128, "%s", g_messages[MSG_UNSUPPORTED]);
 					} else {
@@ -433,7 +418,7 @@ int submenu_draw(void)
 					if (temp > width)
 						width = temp;
 					// if model is not PSPGo, check the "no hibernation support" string width too
-					if (psp_model != PSP_GO && i == SUBMENU_DELETE_HIBERNATION) {
+					if (g_vsh_menu->psp_model != PSP_GO && i == SUBMENU_DELETE_HIBERNATION) {
 						temp = scePaf_strlen(g_messages[MSG_NO_HIBERNATION]);
 						if (temp > width)
 							width = temp;
@@ -441,7 +426,7 @@ int submenu_draw(void)
 				}
 				
 				// center-justify submenu options
-				if (psp_model != PSP_GO && submax_menu == SUBMENU_DELETE_HIBERNATION) {
+				if (g_vsh_menu->psp_model != PSP_GO && submax_menu == SUBMENU_DELETE_HIBERNATION) {
 					// hibernation mode unsupported if model is not PSP Go
 					padding = (width - scePaf_strlen(g_messages[MSG_NO_HIBERNATION])) / 2;
 					scePaf_snprintf(msg, 128, " %*s%s%*s ", padding, "", g_messages[MSG_NO_HIBERNATION], padding, "");
@@ -460,81 +445,64 @@ int submenu_draw(void)
 }
 
 
-int submenu_setup(void)
-{
+int submenu_setup(void) {
 	int i;
 	const char *bridge;
 	const char *umdvideo_disp;
 
 	// preset
-	for(i=0;i<SUBMENU_MAX;i++) {
+	for (i = 0; i < SUBMENU_MAX; i++) {
 		subitem_str[i] = NULL;
 		item_fcolor[i] = RGB(255,255,255);
 	}
 	
-
-
-//usb device
-	if((cnf.usbdevice>0) && (cnf.usbdevice<5)) {
-		scePaf_sprintf(device_buf, "%s %d", g_messages[MSG_FLASH], cnf.usbdevice-1);
+	//usb device
+	if ((g_vsh_menu->config.se.usbdevice > 0) && (g_vsh_menu->config.se.usbdevice < 5)) {
+		scePaf_sprintf(device_buf, "%s %d", g_messages[MSG_FLASH], g_vsh_menu->config.se.usbdevice - 1);
 		bridge = device_buf;
-	} else if (IS_VITA_ADR(ark_config)) {
+	} else if (IS_VITA_ADR(g_vsh_menu->config.p_ark)) {
 		scePaf_sprintf(device_buf, "%s", g_messages[MSG_USE_ADRENALINE_SETTINGS]);
 		bridge = device_buf;
-	}else {
+	} else {
 		const char *device;
-
-		if(cnf.usbdevice==5)
+		if(g_vsh_menu->config.se.usbdevice==5)
 			device= g_messages[MSG_UMD_DISC];
-		else if(psp_model == PSP_GO)
+		else if(g_vsh_menu->psp_model == PSP_GO)
 			device = g_messages[MSG_INTERNAL_STORAGE];
 		else
 			device = g_messages[MSG_MEMORY_STICK];
 
 		bridge = device;
 	}
+	subitem_str[SUBMENU_USB_DEVICE] = bridge;
 
-	umdvideo_disp = strrchr(umdvideo_path, '/');
+	umdvideo_disp = (const char*)scePaf_strrchr(umdvideo_path, '/');
 
-	if(umdvideo_disp == NULL) {
+	if (umdvideo_disp == NULL)
 		umdvideo_disp = umdvideo_path;
-	} else {
+	else
 		umdvideo_disp++;
-	}
 
-	if(IS_VITA_ADR(ark_config))
+	if (IS_VITA_ADR(g_vsh_menu->config.p_ark))
 		subitem_str[SUBMENU_UMD_VIDEO] = g_messages[MSG_UNSUPPORTED];
 	else
 		subitem_str[SUBMENU_UMD_VIDEO] = umdvideo_disp;
-	subitem_str[SUBMENU_USB_DEVICE] = bridge;
 
-/*
-	switch(cnf.umdmode) {
-		case MODE_NP9660:
-			subitem_str[SUBMENU_UMD_MODE] = g_messages[MSG_NP9660];
-			break;
-		case MODE_INFERNO:
-			subitem_str[SUBMENU_UMD_MODE] = g_messages[MSG_INFERNO];
-			break;
-	}
-*/
-	if(cnf.umdmode == 3) {
+	if (g_vsh_menu->config.se.umdmode == 3)
 		subitem_str[SUBMENU_UMD_MODE] = g_messages[MSG_INFERNO];
-	} else if(cnf.umdmode<2) {
-		cnf.umdmode = 3;
-	} else if(cnf.umdmode>3) {
-		cnf.umdmode = 2;
-	} else {
+	else if (g_vsh_menu->config.se.umdmode < 2)
+		g_vsh_menu->config.se.umdmode = 3;
+	else if (g_vsh_menu->config.se.umdmode > 3)
+		g_vsh_menu->config.se.umdmode = 2;
+	else 
 		subitem_str[SUBMENU_UMD_MODE] = g_messages[MSG_NP9660];
-	}
 
-	if (config.vsh_font)
-		subitem_str[SUBMENU_FONT] = available_fonts[config.vsh_font-1];
-	else{
+	if (g_vsh_menu->config.ark_menu.vsh_font)
+		subitem_str[SUBMENU_FONT] = font_list()[g_vsh_menu->config.ark_menu.vsh_font - 1];
+	else
 		subitem_str[SUBMENU_FONT] = g_messages[MSG_DEFAULT];
-	}
 
-	switch(cnf.usbdevice_rdonly) {
+	switch (g_vsh_menu->config.se.usbdevice_rdonly) {
 		case 0:
 			subitem_str[SUBMENU_USB_READONLY] = g_messages[MSG_DISABLE];
 			break;
@@ -548,7 +516,7 @@ int submenu_setup(void)
 			subitem_str[SUBMENU_USB_READONLY] = g_messages[MSG_ENABLE];
 	}
 
-	switch(swap_xo) {
+	switch (g_vsh_menu->status.swap_xo) {
 		case XO_CURRENT_O_PRIMARY:
 			subitem_str[SUBMENU_SWAP_XO_BUTTONS] = g_messages[MSG_O_PRIM];
 			break;
@@ -559,7 +527,7 @@ int submenu_setup(void)
 			subitem_str[SUBMENU_SWAP_XO_BUTTONS] = g_messages[MSG_X_PRIM]; // should never happen?
 	}
 
-	switch(cur_battery) {
+	switch (g_vsh_menu->battery) {
 		case NORMAL_TO_PANDORA:
 			subitem_str[SUBMENU_CONVERT_BATTERY] = g_messages[MSG_NORMAL_TO_PANDORA];
 			break;
@@ -571,7 +539,7 @@ int submenu_setup(void)
 			break;
 	}
 
-	switch(cnf.vshregion) {
+	switch(g_vsh_menu->config.se.vshregion) {
 		case FAKE_REGION_DISABLED:
 			subitem_str[SUBMENU_REGION_MODE] = g_messages[MSG_DEFAULT];
 			break;
@@ -618,7 +586,7 @@ int submenu_setup(void)
 			subitem_str[SUBMENU_REGION_MODE] = g_messages[MSG_DISABLE];
 	}
 
-	switch(cnf.umdregion) {
+	switch(g_vsh_menu->config.se.umdregion) {
 		case UMD_REGION_DEFAULT:
 			subitem_str[SUBMENU_UMD_REGION_MODE] = g_messages[MSG_DEFAULT];
 			break;
@@ -637,7 +605,7 @@ int submenu_setup(void)
 	}
 	
 
-	switch(config.vsh_fg_color) {
+	switch(g_vsh_menu->config.ark_menu.vsh_fg_color) {
 		case FG_RANDOM:
 			subitem_str[SUBMENU_FG_COLORS] = g_messages[MSG_RANDOM];
 			break;
@@ -728,7 +696,7 @@ int submenu_setup(void)
 		default:
 			subitem_str[SUBMENU_FG_COLORS] = g_messages[MSG_WHITE];
 	}
-	switch(config.vsh_bg_color) {
+	switch(g_vsh_menu->config.ark_menu.vsh_bg_color) {
 		case BG_RANDOM:
 			subitem_str[SUBMENU_BG_COLORS] = g_messages[MSG_RANDOM];
 			break;
@@ -824,12 +792,10 @@ int submenu_setup(void)
 }
 
 
-int submenu_ctrl(u32 button_on)
-{
+int submenu_ctrl(u32 button_on) {
 	int direction;
 
-	if( (button_on & PSP_CTRL_SELECT) ||
-		(button_on & PSP_CTRL_HOME)) {
+	if ((button_on & PSP_CTRL_SELECT) || (button_on & PSP_CTRL_HOME)) {
 		submenu_sel = SUBMENU_GO_BACK;
 		return 1;
 	}
@@ -837,114 +803,149 @@ int submenu_ctrl(u32 button_on)
 	// change menu select
 	direction = 0;
 
-	if(button_on & PSP_CTRL_DOWN) direction++;
-	if(button_on & PSP_CTRL_UP) direction--;
+	if (button_on & PSP_CTRL_DOWN) 
+		direction++;
+	if (button_on & PSP_CTRL_UP) 
+		direction--;
 
-	submenu_sel = limit(submenu_sel+direction, 0, SUBMENU_MAX-1);
+	#define ROLL_OVER(val, min, max) ( ((val) < (min)) ? (max): ((val) > (max)) ? (min) : (val) )
+	submenu_sel = ROLL_OVER(submenu_sel + direction, 0, SUBMENU_MAX - 1);
 
 	// LEFT & RIGHT
 	direction = -2;
 
-	if(button_on & PSP_CTRL_LEFT)   direction = -1;
-	if(button_on & PSP_CTRL_CROSS) direction = 0;
-	if(button_on & PSP_CTRL_CIRCLE) direction = 0;
-	if(button_on & PSP_CTRL_RIGHT)  direction = 1;
+	if(button_on & PSP_CTRL_LEFT)
+		direction = -1;
+	if(button_on & PSP_CTRL_CROSS)
+		direction = 0;
+	if(button_on & PSP_CTRL_CIRCLE)
+		direction = 0;
+	if(button_on & PSP_CTRL_RIGHT) 
+		direction = 1;
 
 	if(direction <= -2)
 		return 0;
 
 	switch(submenu_sel) {
 		case SUBMENU_USB_DEVICE:
-			if (IS_VITA_ADR(ark_config)) break;
-			if(direction) change_usb( direction );
+			if (IS_VITA_ADR(g_vsh_menu->config.p_ark)) 
+				break;
+			if (direction) 
+				change_usb(direction);
 			break;
 		case SUBMENU_USB_READONLY:
-			if (IS_VITA_ADR(ark_config)) break;
-			if (direction) swap_readonly(direction);
+			if (IS_VITA_ADR(g_vsh_menu->config.p_ark)) 
+				break;
+			if (direction) 
+				swap_readonly(direction);
 			break;
 		case SUBMENU_UMD_MODE:
-			if(direction) change_umd_mode( direction );
+			if (direction) 
+				change_umd_mode(direction);
 			break;
 		case SUBMENU_UMD_VIDEO:
-			if (IS_VITA_ADR(ark_config)) break;
-			if(direction) {
+			if (IS_VITA_ADR(g_vsh_menu->config.p_ark)) 
+				break;
+			if (direction) {
 			   	change_umd_mount_idx(direction);
 
 				if(umdvideo_idx != 0) {
 					char *umdpath;
-
 					umdpath = umdvideolist_get(&g_umdlist, umdvideo_idx-1);
 
 					if(umdpath != NULL) {
-						strncpy(umdvideo_path, umdpath, sizeof(umdvideo_path));
+						scePaf_strncpy(umdvideo_path, umdpath, sizeof(umdvideo_path));
 						umdvideo_path[sizeof(umdvideo_path)-1] = '\0';
-					}else {
+					} else
 						goto none;
-					}
 				} else {
 none:
-					strcpy(umdvideo_path, g_messages[MSG_NONE]);
+					scePaf_strcpy(umdvideo_path, g_messages[MSG_NONE]);
 				}
-			} else {
+			} else
 				return 6; // Mount UMDVideo ISO flag
-			}
 			break;
 		case SUBMENU_IMPORT_CLASSIC_PLUGINS:
-			if(direction==0) {
+			if (direction == 0)
 				return 13; // Import Classic Plugins flag 
-			}
 			break;
 		case SUBMENU_DELETE_HIBERNATION:
-			if(direction==0) {
+			if (direction == 0)
 				return 10; // Delete Hibernation flag 
-			}
 			break;
 		case SUBMENU_RANDOM_GAME:
-			if(direction==0) {
+			if (direction == 0)
 				return 14; // Random Game flag 
-			}
 			break;
 		case SUBMENU_ACTIVATE_FLASH_WMA:
-			if(direction==0) {
+			if (direction == 0)
 				return 11; // Activate Flash/WMA flag 
-			}
 			break;
 		case SUBMENU_REGION_MODE:
-			if(direction) change_region ( direction, 13 );
+			if (direction) 
+				change_region(direction, 13);
 			break;
 		case SUBMENU_UMD_REGION_MODE:
-			if(psp_model == PSP_GO || IS_VITA_ADR(ark_config)) break;
-			if(direction) change_umd_region ( direction, 3 );
+			if (g_vsh_menu->psp_model == PSP_GO || IS_VITA_ADR(g_vsh_menu->config.p_ark)) 
+				break;
+			if (direction) 
+				change_umd_region(direction, 3);
 			break;
 		case SUBMENU_SWAP_XO_BUTTONS:
-			if (direction==0) {
+			if (direction == 0)
 				return 12; // Swap X/O Buttons flag  
-			} 
 			break;
 		case SUBMENU_CONVERT_BATTERY:
-			if(direction==0) {
+			if(direction == 0)
 				return 9; // Convert Battery flag
-			}
 			break;
 		case SUBMENU_FG_COLORS:
 			// This will be where I will be adding to set the color
-			if(direction) change_fg_color(direction);
+			if (direction)
+				change_fg_color(direction);
 			break;
 		case SUBMENU_BG_COLORS:
 			// This will be where I will be adding to set the color
-			if(direction) change_bg_color(direction);
+			if(direction) 
+				change_bg_color(direction);
 			break;
 		case SUBMENU_FONT:
-			if (direction){
+			if (direction) {
 				change_font(direction);
 				release_font();
-				load_selected_font();
+				font_load(g_vsh_menu);
 			}
 			break;
 		case SUBMENU_GO_BACK:
-			if(direction==0) return 1; // finish
+			if(direction==0) 
+				return 1; // finish
 			break;
 	}
 
 	return 0; // continue
+}
+
+
+void subbutton_func(vsh_Menu *vsh) {
+	int res;
+	// submenu control
+	switch(vsh->status.submenu_mode) {
+		case 0:	
+			if ((vsh->buttons.pad.Buttons & ALL_CTRL) == 0)
+				vsh->status.submenu_mode = 1;
+			break;
+		case 1:
+			res = submenu_ctrl(vsh->buttons.new_buttons_on);
+
+			if (res != 0) {
+				sub_stop_stock = res;
+				vsh->status.submenu_mode = 2;
+			}
+			break;
+		case 2: // exit waiting 
+			// exit submenu
+			if ((vsh->buttons.pad.Buttons & ALL_CTRL) == 0)
+				vsh->status.sub_stop_flag = sub_stop_stock;
+			break;
+	}
 }
