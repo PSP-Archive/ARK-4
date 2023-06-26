463f1c52183383b5001d7853073bce5006444ecf
pyroesp <nicolaslopezesp@gmail.com>
2023-06-27 01:17:26 +0200

    blit: make struct to remove globals
    + move font stuff

--- extras/menus/advancedvsh/include/blit.h
+++ extras/menus/advancedvsh/include/blit.h
@@ -18,6 +18,10 @@
 #ifndef __BLIT_H__
 #define __BLIT_H__
 
+
+#include <psptypes.h>
+
+
 #define COLOR_CYAN	0x00ffff00
 #define COLOR_MAGENDA 0x00ff00ff
 #define COLOR_YELLOW  0x0000ffff
@@ -25,8 +29,20 @@
 #define RGB(R,G,B)	(((B)<<16)|((G)<<8)|(R))
 #define RGBT(R,G,B,T) (((T)<<24)|((B)<<16)|((G)<<8)|(R))
 
-#define CENTER(num) ((480/2)-(num*(8/2)))
 
+typedef struct _blit_Gfx{
+	u32 *vram32;
+	u32 fg_color, bg_color;
+	
+	int width, height;
+	
+	int bufferwidth;
+	int pixelformat;
+}blit_Gfx;
+
+
+
+blit_Gfx* blit_gfx_pointer(void);
 int blit_setup(void);
 void blit_set_color(int fg_col, int bg_col);
 int blit_string(int sx, int sy, const char *msg);
