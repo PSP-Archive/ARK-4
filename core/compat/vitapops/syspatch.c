00cf1fc743da20ef24d7c9e9b1d05ea08a0d209e
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-07-03 14:17:24 +0200

    added support for cwcheat in vitapops

--- core/compat/vitapops/syspatch.c
+++ core/compat/vitapops/syspatch.c
@@ -10,11 +10,19 @@
 #include <systemctrl_private.h>
 #include <pspiofilemgr.h>
 #include <pspgu.h>
+#include <pspinit.h>
 #include <functions.h>
 #include "popsdisplay.h"
 
 extern ARKConfig* ark_config;
 extern STMOD_HANDLER previous;
+
+static int draw_thread = -1;
+static int do_draw = 0;
+static u32* g_vram_base = (u32*)0x44000000;
+int (* DisplaySetFrameBuf)(void*, int, int, int) = NULL;
+int (*DisplayWaitVblankStart)() = NULL;
+
 
 KernelFunctions _ktbl = {
     .KernelDcacheInvalidateRange = &sceKernelDcacheInvalidateRange,
@@ -62,12 +70,56 @@ void patchVitaPopsDisplay(SceModule2* mod){
     }
 }
 
+int pops_draw_thread(int argc, void* argp){
+    
+    while (do_draw){
+        SoftRelocateVram(g_vram_base, NULL);
+        DisplayWaitVblankStart();
+    }
+    
+    return 0;
+}
+
+int sceKernelSuspendThreadPatched(SceUID thid) {
+	SceKernelThreadInfo info;
+	info.size = sizeof(SceKernelThreadInfo);
+	if (sceKernelReferThreadStatus(thid, &info) == 0) {
+		if (strcmp(info.name, "popsmain") == 0) {
+            if (draw_thread < 0){
+                do_draw = 1;
+			    draw_thread = sceKernelCreateThread("psxloader", &pops_draw_thread, 0x10, 0x10000, PSP_THREAD_ATTR_VFPU, NULL);
+                sceKernelStartThread(draw_thread, 0, NULL);
+            }
+		}
+	}
+
+	return sceKernelSuspendThread(thid);
+}
+
+int sceKernelResumeThreadPatched(SceUID thid) {
+	SceKernelThreadInfo info;
+	info.size = sizeof(SceKernelThreadInfo);
+	if (sceKernelReferThreadStatus(thid, &info) == 0) {
+		if (strcmp(info.name, "popsmain") == 0) {
+			if (draw_thread >= 0){
+                do_draw = 0;
+                sceKernelWaitThreadEnd(draw_thread, NULL);
+                draw_thread = -1;
+            }
+		}
+	}
+
+	return sceKernelResumeThread(thid);
+}
+
 void ARKVitaPopsOnModuleStart(SceModule2 * mod){
 
     static int booted = 0;
     
     // Patch display in PSX exploits
     if(strcmp(mod->modname, "sceDisplay_Service") == 0) {
+        DisplaySetFrameBuf = (void*)sctrlHENFindFunction("sceDisplay_Service", "sceDisplay", 0x289D82FE);
+        DisplayWaitVblankStart = (void*)sctrlHENFindFunction("sceDisplay_Service", "sceDisplay", 0x984C27E7);
         patchVitaPopsDisplay(mod);
         goto flush;
     }
@@ -78,6 +130,14 @@ void ARKVitaPopsOnModuleStart(SceModule2 * mod){
         patchKermitPeripheral(&_ktbl);
         goto flush;
     }
+
+    if (strcmp(mod->modname, "CWCHEATPRX") == 0) {
+		if (sceKernelInitKeyConfig() == PSP_INIT_KEYCONFIG_POPS) {
+			hookImportByNID(mod, "ThreadManForKernel", 0x9944F31F, sceKernelSuspendThreadPatched);
+			hookImportByNID(mod, "ThreadManForKernel", 0x75156E8F, sceKernelResumeThreadPatched);
+			goto flush;
+		}
+	}
 
     // Boot Complete Action not done yet
     if(booted == 0)
