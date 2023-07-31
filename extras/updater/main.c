deb4b99c1bd4315831478bed3315526134d172d2
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-07-31 15:27:17 +0200

    fix mkdir

--- extras/updater/main.c
+++ extras/updater/main.c
@@ -59,12 +59,12 @@ void checkArkConfig(ARKConfig* ark_config){
     if (strcmp(ark_config->arkpath, "ms0:/SEPLUGINS/") == 0 || sceIoGetstat(ark_config->arkpath, &stat) < 0){
         // create savedata folder, first attempt on ef0 for PSP Go
         strcpy(ark_config->arkpath, "ef0:/PSP/SAVEDATA/ARK_01234/");
-        int res = sceIoMkdir(ark_config->arkpath, 0777);
+        int res = sceIoMkdir("ef0:/PSP/SAVEDATA/ARK_01234", 0777);
         if (res < 0){
             // second attempt on ms0 for every other device
             ark_config->arkpath[0] = 'm';
             ark_config->arkpath[1] = 's';
-            res = sceIoMkdir(ark_config->arkpath, 0777);
+            res = sceIoMkdir("ms0:/PSP/SAVEDATA/ARK_01234", 0777);
         }
         // creation worked?
         if (res >= 0){
