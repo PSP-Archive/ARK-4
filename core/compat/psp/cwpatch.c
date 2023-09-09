bcc160e18c8c270b192907ba4257527f56cf52d8
Brennen Murphy <brennenmurph@pm.me>
2023-09-09 16:23:41 -0400

    oops

---
+++ core/compat/psp/cwpatch.c
@@ -0,0 +1,27 @@
+
+#include <pspsdk.h>
+#include <pspinit.h>
+#include <pspdisplay.h>
+#include <stdio.h>
+
+
+int (* DisplayGetFrameBuf)(void*, int, int, int) = NULL;
+
+// CWCHEAT Patch
+int sceKernelSuspendThreadPatched(SceUID thid) {
+	SceKernelThreadInfo info;
+	info.size = sizeof(SceKernelThreadInfo);
+	if(sceKernelReferThreadStatus(thid, &info) == 0) {
+        if (strcmp(info.name, "popsmain") == 0) {
+            //void* framebuf = NULL;
+			void *framebuf;
+            int width;
+			int pixelformat;
+
+			DisplayGetFrameBuf = (void*)sctrlHENFindFunction("sceDisplay_Service", "sceDisplay", 0xEEDA2E54);
+            DisplayGetFrameBuf(&framebuf, &width, &pixelformat, 0);
+            memset(framebuf, 0, 512 * 272 * 4);
+		}
+	}
+    return sceKernelSuspendThread(thid);
+}
