a0588c46b34e02f7bafc2640ef47c7be04aba2df
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2024-02-05 16:09:55 +0100

    cleanup

--- loader/live/kernel/psxloader/ps1cfw_enabler/ps1cfw_enabler.c
+++ loader/live/kernel/psxloader/ps1cfw_enabler/ps1cfw_enabler.c
@@ -40,31 +40,13 @@ int (* ScePspemuDivide)(uint64_t x, uint64_t y);
 int (* ScePspemuErrorExit)(int error);
 int (* ScePspemuConvertAddress)(uint32_t addr, int mode, uint32_t cache_size);
 int (* ScePspemuWritebackCache)(void *addr, int size);
-int (* ScePspemuInitPocs)();
 
 void get_functions(uint32_t text_addr) {
   ScePspemuDivide                     = (void *)(text_addr + 0x39F0 + 0x1);
   ScePspemuErrorExit                  = (void *)(text_addr + 0x4104 + 0x1);
   ScePspemuConvertAddress             = (void *)(text_addr + 0x6364 + 0x1);
   ScePspemuWritebackCache             = (void *)(text_addr + 0x6490 + 0x1);
-  
-  ScePspemuInitPocs                   = (void *)(text_addr + 0x30678 + 0x1);
 }
-
-// Tile ID patched
-char *ScePspemuGetTitleidPatched() {
-  if (popsconfig.magic == ARK_MAGIC && popsconfig.title_id[0])
-      return popsconfig.title_id;
-  return TAI_CONTINUE(char*, ScePspemuGetTitleidRef);
-}
-
-#if 0
-void logtext(char* text){
-    SceUID fd = sceIoOpen("ux0:pspemu/ps1log.txt", SCE_O_WRONLY|SCE_O_CREAT|SCE_O_APPEND, 0777);
-    sceIoWrite(fd, text, strlen(text));
-    sceIoClose(fd);
-}
-#endif
 
 // IO Open patched
 SceUID sceIoOpenPatched(const char *file, int flags, SceMode mode) {
@@ -113,13 +95,6 @@ SceUID sceIoOpenPatched(const char *file, int flags, SceMode mode) {
         ScePspemuErrorExit(0);
         return 0;
     }
-    
-    // unlock PS button when boot complete
-    if (strstr(file, "__popsbooted__")){
-        sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN);
-        sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
-        return -103;
-    }
 
     // Redirect files for memory card manager
     if (popsconfig.magic == ARK_MAGIC && popsconfig.title_id[0] && popsconfig.path[0]){
@@ -140,7 +115,7 @@ SceUID sceIoOpenPatched(const char *file, int flags, SceMode mode) {
               file = new_file;
             }
           }
-        } else if (strstr(file, "pspemu/PSP/SAVEDATA/") &&
+        } else if (strstr(file, "/SCPS10084/") &&
                   (strcmp(p+1, "PARAM.SFO") == 0 ||
                    strcmp(p+1, "SCEVMC0.VMP") == 0 ||
                    strcmp(p+1, "SCEVMC1.VMP") == 0)) {
@@ -158,7 +133,7 @@ int sceIoGetstatPatched(const char *file, SceIoStat *stat) {
       if (p) {
         static char new_file[256];
 
-        if (strstr(file, "pspemu/PSP/SAVEDATA/") &&
+        if (strstr(file, "/SCPS10084/") &&
            (strcmp(p+1, "PARAM.SFO") == 0 ||
             strcmp(p+1, "SCEVMC0.VMP") == 0 ||
             strcmp(p+1, "SCEVMC1.VMP") == 0)) {
@@ -199,70 +174,6 @@ int module_start(SceSize argc, const void *args) {
     // fix memory card manager
     sceIoStatHook = taiHookFunctionImport(&sceIoGetstatRef, "ScePspemu", 0xCAE9ACE6, 0xBCA5B623, sceIoGetstatPatched);
 
-    if (module_nid == 0x2714F07D) { // 3.60 retail
-        titleIdHook = taiHookFunctionOffset(&ScePspemuGetTitleidRef, info.modid, 0, 0x205FC, 0x1, ScePspemuGetTitleidPatched);
-    }
-    else if (module_nid == 0x3F75D4D3) { // 3.65-3.70 retail
-        titleIdHook = taiHookFunctionOffset(&ScePspemuGetTitleidRef, info.modid, 0, 0x20600, 0x1, ScePspemuGetTitleidPatched);
-    }
-
-    #if 0
-    uint32_t movs_a4_1_nop_opcode = 0xBF002301;
-    uint32_t movs_a1_0_nop_opcode = 0xBF002000;
-    uint32_t movs_a1_1_nop_opcode = 0xBF002001;
-    uint32_t uids[64]; int n_uids = 0;
-    
-        // Resume stuff. PROBABLY SHOULD DO POPS AND PSP MODE STUFF
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x42F0, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    // Unknown. Mode 4, 5
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x572E, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    // Set cache address for pops stuff
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x57C0, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    // Read savedata and menu info. Should be enabled, otherwise an error will occur
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x5BBA, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    // Get app state for pops
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x5C52, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    // Unknown
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x5E4A, &movs_a4_1_nop_opcode, sizeof(movs_a4_1_nop_opcode));
-
-    ///////////////////////////
-
-    // isPops patches
-
-    // Peripheral
-
-    // Use vibration
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x169F6, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // Unknown check for POPS mode
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x16AEC, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // Unknown check for PSP mode. If false return 0x80010089
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x16B6C, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // Unknown check for PSP mode. If false return 0
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x16B86, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // Unknown check for PSP mode. If false return 0
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x16C3E, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    ////////////////////
-
-    // Init ScePspemuMenuWork
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x1825E, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // Read savedata and menu info
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x2121E, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-
-    // POPS Settings menu function
-    uids[n_uids++] = taiInjectData(info.modid, 0, 0x17B32, &movs_a1_1_nop_opcode, sizeof(movs_a1_1_nop_opcode));
-    #endif
-
     return SCE_KERNEL_START_SUCCESS;
 }
 
@@ -270,7 +181,6 @@ int module_stop(SceSize argc, const void *args) {
 
   if(sceIoOpenHook >= 0) taiHookRelease(sceIoOpenHook, sceIoOpenRef);
   if (sceIoStatHook >= 0) taiHookRelease(sceIoStatHook, sceIoGetstatRef);
-  if (titleIdHook >= 0) taiHookRelease(titleIdHook, ScePspemuGetTitleidRef);
   if (io_patch_path) taiInjectRelease(io_patch_path);
   if (io_patch_size) taiInjectRelease(io_patch_size);
 
