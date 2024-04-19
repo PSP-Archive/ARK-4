09cd1676b1be2e70f270b683d437027f3e0e4b91
Brennen Murphy <brennenmurph@pm.me>
2024-04-19 11:26:23 -0400

    Added flashing IPL only to DC-ARK-Maker

--- contrib/PC/MagicMemoryCreator/main.py
+++ contrib/PC/MagicMemoryCreator/main.py
@@ -34,6 +34,7 @@ var = tk.StringVar(m)
 var.set(possible_drive[0])
 check = tk.BooleanVar(m)
 format_ms_check = tk.BooleanVar(m)
+ipl_only = tk.BooleanVar(m)
 
 disk_check = tk.StringVar(m)
 disk_check.set(0)
@@ -63,6 +64,16 @@ else:
     out = out.stdout.read().decode().splitlines()
     for i in out:
         possible_drive.append(i)
+
+def toggle_ipl():
+    if ipl_only.get():
+        go_check['state'] = 'disabled'
+        format_ms['state'] = 'disabled'
+        m.update()
+    else:
+        go_check['state'] = 'normal'
+        format_ms['state'] = 'normal'
+        m.update()
 
 def refresh():
     m.destroy()
@@ -128,18 +139,20 @@ def fmt_ms():
                 os.system(f'sudo mkfs.fat -F 32 /dev/{var.get()}1')
 
             time.sleep(3)
-            os.system(f'mount /dev/{var.get()}1 /mnt')
-            os.system(f'mount -o remount,rw /mnt')
+            users = subprocess.Popen(["users | awk '{print $1}'"], shell=True, stdout=subprocess.PIPE)
+            users = users.stdout.read().decode().strip()
+            os.system(f"su -c 'udisksctl mount -b /dev/{var.get()}1' {users}")
+            #os.system(f'mount /dev/{var.get()}1 /mnt')
+            #os.system(f'mount -o remount,rw /mnt')
             time.sleep(3)
             refresh()
 
     legacy['state'] = 'disabled'
     go_check['state'] = 'disabled'
+    ipl_inject_only['state'] = 'disabled'
     status.config(text='Formatting Memory Stick\nSelected!') 
     b.config(text='Format', command=fmt)
     m.update()
-    
-    
 
 
 def disable_go_check():
@@ -179,7 +192,7 @@ def go_update():
 def toggle_run(toggle) -> None:
     if toggle != '-':
         b['state'] = 'normal'
-        format_ms.grid(row=4, column=0, sticky="W")
+        format_ms.grid(row=5, column=0, sticky="W")
     else:
         b['state'] = 'disabled'
         format_ms.grid_remove()
@@ -191,76 +204,79 @@ def run() -> None:
     go_check['state'] = 'disabled'
     legacy['state'] = 'disabled'
     format_ms['state'] = 'disabled'
+    ipl_inject_only['state'] = "disabled"
     global go
 
-    # Download pspdecrypt from John
-    if ostype == 'Linux':
-        resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-linux.zip', timeout=10, verify=False)
-        with open('pspdecrypt-1.0-linux.zip', 'wb') as f:
-            f.write(resp.content)
-            resp.close()
-        with ZipFile('pspdecrypt-1.0-linux.zip', 'r') as zObject:
-            zObject.extractall(path=f'{os.getcwd()}/')
-        os.system('chmod 755 pspdecrypt')
-        x['state'] = "normal"
-    elif ostype == 'Windows':
-        resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-windows.zip', timeout=10, verify=False)
-        with open('pspdecrypt-1.0-windows.zip', 'wb') as f:
-            f.write(resp.content)
-            resp.close()
-        with ZipFile('pspdecrypt-1.0-windows.zip', 'r') as zObject:
-            zObject.extractall(path=f'{os.getcwd()}\\')
-        os.system('oschmod 755 pspdecrypt.exe')
-        x['state'] = "normal"
-    elif ostype == 'Darwin':
-        resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-macos.zip', timeout=10, verify=False)
-        with open('pspdecrypt-1.0-macos.zip', 'wb') as f:
-            f.write(resp.content)
-            resp.close()
-        with ZipFile('pspdecrypt-1.0-macos.zip', 'r') as zObject:
-            zObject.extractall(path=f'{os.getcwd()}/')
-        os.system('oschmod 755 pspdecrypt')
-        x['state'] = "normal"
-    else:
-        print('\nERR: unsupported platform...\n')
-        return
+    if not ipl_only.get():
 
-    # Download 6.61 OFW
-    if go:
-        resp = requests.get('http://du01.psp.update.playstation.org/update/psp/image2/us/2014_1212_fd0f7d0798b4f6e6d32ef95836740527/EBOOT.PBP', timeout=10, verify=False)
-        if resp:
-            with open('661_GO.PBP', 'wb') as f:
+        # Download pspdecrypt from John
+        if ostype == 'Linux':
+            resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-linux.zip', timeout=10, verify=False)
+            with open('pspdecrypt-1.0-linux.zip', 'wb') as f:
                 f.write(resp.content)
                 resp.close()
-        else:
-            print(resp.status_code)
-    else:
-        resp = requests.get('http://du01.psp.update.playstation.org/update/psp/image/us/2014_1212_6be8878f475ac5b1a499b95ab2f7d301/EBOOT.PBP', timeout=10, verify=False)
-        if resp:
-            with open('661.PBP', 'wb') as f:
+            with ZipFile('pspdecrypt-1.0-linux.zip', 'r') as zObject:
+                zObject.extractall(path=f'{os.getcwd()}/')
+            os.system('chmod 755 pspdecrypt')
+            x['state'] = "normal"
+        elif ostype == 'Windows':
+            resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-windows.zip', timeout=10, verify=False)
+            with open('pspdecrypt-1.0-windows.zip', 'wb') as f:
                 f.write(resp.content)
                 resp.close()
+            with ZipFile('pspdecrypt-1.0-windows.zip', 'r') as zObject:
+                zObject.extractall(path=f'{os.getcwd()}\\')
+            os.system('oschmod 755 pspdecrypt.exe')
+            x['state'] = "normal"
+        elif ostype == 'Darwin':
+            resp = requests.get('https://github.com/John-K/pspdecrypt/releases/download/1.0/pspdecrypt-1.0-macos.zip', timeout=10, verify=False)
+            with open('pspdecrypt-1.0-macos.zip', 'wb') as f:
+                f.write(resp.content)
+                resp.close()
+            with ZipFile('pspdecrypt-1.0-macos.zip', 'r') as zObject:
+                zObject.extractall(path=f'{os.getcwd()}/')
+            os.system('oschmod 755 pspdecrypt')
+            x['state'] = "normal"
         else:
-            print(resp.status_code)
+            print('\nERR: unsupported platform...\n')
+            return
 
-    if ostype == 'Linux' or ostype == 'Darwin':
+        # Download 6.61 OFW
         if go:
-            os.system('./pspdecrypt -e 661_GO.PBP')
-            shutil.copytree("661_GO/F0", "TM/DCARK", dirs_exist_ok=True)
-            shutil.copytree("661_GO/F1", "TM/DCARK", dirs_exist_ok=True)
+            resp = requests.get('http://du01.psp.update.playstation.org/update/psp/image2/us/2014_1212_fd0f7d0798b4f6e6d32ef95836740527/EBOOT.PBP', timeout=10, verify=False)
+            if resp:
+                with open('661_GO.PBP', 'wb') as f:
+                    f.write(resp.content)
+                    resp.close()
+            else:
+                print(resp.status_code)
         else:
-            os.system('./pspdecrypt -e 661.PBP')
-            shutil.copytree("661/F0", "TM/DCARK", dirs_exist_ok=True)
-            shutil.copytree("661/F1", "TM/DCARK", dirs_exist_ok=True)
-    else:
-        if go:
-            os.system('.\\pspdecrypt.exe -e 661_GO.PBP')
-            shutil.copytree("661_GO\\F0\\", "TM\\DCARK\\", dirs_exist_ok=True)
-            shutil.copytree("661_GO\\F1\\", "TM\\DCARK\\", dirs_exist_ok=True)
+            resp = requests.get('http://du01.psp.update.playstation.org/update/psp/image/us/2014_1212_6be8878f475ac5b1a499b95ab2f7d301/EBOOT.PBP', timeout=10, verify=False)
+            if resp:
+                with open('661.PBP', 'wb') as f:
+                    f.write(resp.content)
+                    resp.close()
+            else:
+                print(resp.status_code)
+
+        if ostype == 'Linux' or ostype == 'Darwin':
+            if go:
+                os.system('./pspdecrypt -e 661_GO.PBP')
+                shutil.copytree("661_GO/F0", "TM/DCARK", dirs_exist_ok=True)
+                shutil.copytree("661_GO/F1", "TM/DCARK", dirs_exist_ok=True)
+            else:
+                os.system('./pspdecrypt -e 661.PBP')
+                shutil.copytree("661/F0", "TM/DCARK", dirs_exist_ok=True)
+                shutil.copytree("661/F1", "TM/DCARK", dirs_exist_ok=True)
         else:
-            os.system('.\\pspdecrypt.exe -e 661.PBP')
-            shutil.copytree("661\\F0\\", "TM\\DCARK\\", dirs_exist_ok=True)
-            shutil.copytree("661\\F1\\", "TM\\DCARK\\", dirs_exist_ok=True)
+            if go:
+                os.system('.\\pspdecrypt.exe -e 661_GO.PBP')
+                shutil.copytree("661_GO\\F0\\", "TM\\DCARK\\", dirs_exist_ok=True)
+                shutil.copytree("661_GO\\F1\\", "TM\\DCARK\\", dirs_exist_ok=True)
+            else:
+                os.system('.\\pspdecrypt.exe -e 661.PBP')
+                shutil.copytree("661\\F0\\", "TM\\DCARK\\", dirs_exist_ok=True)
+                shutil.copytree("661\\F1\\", "TM\\DCARK\\", dirs_exist_ok=True)
 
     # Download msipl_installer from Draan (forked for macOS support)
     #resp = requests.get('https://raw.githubusercontent.com/krazynez/msipl_installer/main/msipl_installer.py', verify=False)
@@ -273,7 +289,8 @@ def run() -> None:
         get_mountpoint = str(get_mountpoint.stdout.read().decode().rstrip()) + "/TM/"
         status.config(text="COPYING PLEASE WAIT!")
         m.update()
-        shutil.copytree("TM", get_mountpoint, dirs_exist_ok=True)
+        if not ipl_only.get():
+            shutil.copytree("TM", get_mountpoint, dirs_exist_ok=True)
         msipl_installer.main(msipl_installer.Args(f'{var.get()}', False, None, False, True ))
         if check.get():
             msipl_installer.main(msipl_installer.Args(f'{var.get()}', False, 'tm_msipl_legacy.bin', False, False ))
@@ -295,14 +312,16 @@ def run() -> None:
         status.config(text="COPYING PLEASE WAIT!")
         m.update()
         subprocess.run(['mount', '-t', 'msdos', '-o', 'rw', f'/dev/{var.get()}s1', get_mountpoint])
-        shutil.copytree("TM", f"{copypoint}", dirs_exist_ok=True)
+        if not ipl_only.get():
+            shutil.copytree("TM", f"{copypoint}", dirs_exist_ok=True)
         subprocess.run(['diskutil', 'umountDisk', 'force', f'/dev/{var.get()}'])
         status.config(fg='green', text="DONE!")
     else:
         get_mountpoint = windows_disk_letter[var.get()] + ":\\TM\\"
         status.config(text="COPYING PLEASE WAIT!")
         m.update()
-        shutil.copytree("TM", f"{get_mountpoint}", dirs_exist_ok=True)
+        if not ipl_only.get():
+            shutil.copytree("TM", f"{get_mountpoint}", dirs_exist_ok=True)
         #os.system('oschmod 755 msipl_installer.py')
         #os.system(f'python .\\msipl_installer.py --pdisk {int(deviceID[var.get()][-1])} --clear')
         #os.system(f'python .\\msipl_installer.py --pdisk {int(deviceID[var.get()][-1])} --insert msipl.bin')
@@ -317,7 +336,8 @@ def run() -> None:
         b['text'] = "DONE!"
     x['state'] = "normal"
 
-    cleanup()
+    if not ipl_only.get():
+        cleanup()
 
 if ostype == 'Linux' or ostype == 'Darwin':
     if os.geteuid() != 0:
@@ -330,7 +350,7 @@ else:
 
 
 # Setup
-m.minsize(320, 180)
+m.minsize(320, 230)
 
 disk = tk.OptionMenu(m, var, *possible_drive, command=toggle_run)
 disk.grid(row=0,column=1)
@@ -341,11 +361,13 @@ status.grid(row=1, column=0)
 #internal=tk.Checkbutton(m, text='Show Internal Disk', variable=disk_check, onvalue=1, offvalue=0)
 #internal.grid(row=3, column=1)
 
+
+
 go_check=tk.Checkbutton(m, text='PSP GO Model (ONLY!)', command=go_update)
-go_check.grid(sticky="W", row=2, column=0)
+go_check.grid(sticky="W", row=3, column=0)
 
 legacy=tk.Checkbutton(m, text="Legacy IPL ( 1K' and early 2K's ONLY )", variable=check, command=disable_go_check)
-legacy.grid(row=3, column=0)
+legacy.grid(row=4, column=0)
 
 
 
@@ -355,7 +377,7 @@ format_ms=tk.Checkbutton(m, text='Format Memory Stick', variable=format_ms_check
 b=tk.Button(m, text='Run', command=run)
 b.grid(row=1,column=1)
 r=tk.Button(m, text='Refresh', command=refresh)
-r.grid(row=5, column=0)
+r.grid(row=6, column=0, sticky='S')
 
 
 x=tk.Button(m, text='Exit', command=m.destroy)
@@ -364,7 +386,8 @@ x.grid(row=2,column=1)
 if var.get() == '-':
     b['state'] = 'disabled'
 
-   
+ipl_inject_only=tk.Checkbutton(m, text='Inject IPL (ONLY!)', variable=ipl_only, command=toggle_ipl)
+ipl_inject_only.grid(row=2, column=0, sticky='W')
 
 m.mainloop()
 
