ed2c26a8bc4ae563921899fc23102985b94c63b5
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2024-12-02 23:44:49 +0100

    implement lock

--- extras/modules/fatfs/main.c
+++ extras/modules/fatfs/main.c
@@ -9,6 +9,7 @@
 #include <pspumd.h>
 #include <psprtc.h>
 #include <pspinit.h>
+#include <pspthreadman.h>
 #include "systemctrl_private.h"
 #include <ark.h>
 #include "macros.h"
@@ -35,6 +36,8 @@ PspSysEventHandler g_power_event = {
     .handler = &power_event_handler,
 };
 
+static SceUID sema_lock = -1;
+
 unsigned int cpu_suspend_interrupts(void){
     return sceKernelCpuSuspendIntr();
 }
@@ -43,12 +46,13 @@ void cpu_resume_interrupts(unsigned int mask){
     sceKernelCpuResumeIntr(mask);
 }
 
-void _fs_lock(){
 
+void _fs_lock(){
+    sceKernelWaitSema(sema_lock, 1, NULL);
 }
 
 void _fs_unlock(){
-
+    sceKernelSignalSema(sema_lock, 1);
 }
 
 void get_fattime(){
@@ -76,6 +80,9 @@ int module_start(SceSize args, void* argp)
 
     ret = sceIoAddDrv(&fatms_drv);
     if (ret < 0) return ret;
+
+    sema_lock = sceKernelCreateSema("fatfs lock",  0, 1, 1, NULL);
+    if (sema_lock < 0) return sema_lock;
 
     return 0;
 }
