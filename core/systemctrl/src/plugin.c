63376bbfd73a91015ba04726e93d94fb764e0b63
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-02-09 18:04:50 +0100

    fix

--- core/systemctrl/src/plugin.c
+++ core/systemctrl/src/plugin.c
@@ -484,9 +484,6 @@ static void settingsHandler(char* path, u8 enabled){
     else if (strcasecmp(path, "noanalog") == 0){
         se_config.noanalog = enabled;
     }
-    else if (strcasecmp(path, "skiplogos") == 0){
-        se_config.skiplogos = enabled;
-    }
     else if (strcasecmp(path, "region_jp") == 0){
         se_config.umdregion = (enabled)?UMD_REGION_JAPAN:0;
     }
