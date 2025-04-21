0fc45ec0354353bb6d06202dbdb775988e6a8431
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-04-21 19:23:28 +0200

    actual fix

--- core/inferno/isoread.c
+++ core/inferno/isoread.c
@@ -128,11 +128,20 @@ static void wait_until_ms0_ready(void)
     int ret, status = 0;
 
     if (sceKernelInitKeyConfig() == PSP_INIT_KEYCONFIG_VSH) return; // no wait on VSH
-    if (strncmp(g_iso_fn, "ms0:", 4) != 0) return; // no wait on other devices (e.g. ef0)
 
-    while( 1 ) {
-        ret = sceIoDevctl("mscmhc0:", 0x02025801, 0, 0, &status, sizeof(status));
-        
+    const char *drvname = (
+        (g_iso_fn[0] == 'm' || g_iso_fn[0] == 'M') &&
+        (g_iso_fn[1] == 's' || g_iso_fn[1] == 'S')
+    )? "mscmhc0:":"mscmhcemu0:";
+
+    while (1) {
+        ret = sceIoDevctl(drvname, 0x02025801, 0, 0, &status, sizeof(status));
+
+        if (ret<0){
+            sceKernelDelayThread(20000);
+            continue;
+        }
+
         if(status == 4) {
             break;
         }
