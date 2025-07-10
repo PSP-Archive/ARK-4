611b07819c847a0e8c892020548d1bcc30543342
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-07-10 23:55:21 +0200

    fixed some images

--- libs/libya2d/ya2d_image.c
+++ libs/libya2d/ya2d_image.c
@@ -348,7 +348,8 @@ exit_error:
 static int get_JPEG_info(u8* data, int data_size, int* out_w, int* out_h){
     int w = 0, h = 0;
     const uint8_t * buf = &data[0];
-    for (int i = 2; i < data_size;) {
+    int i = 2
+    for (; i < data_size;) {
         if (buf[i] == 0xFF) {
             i++;
             switch(buf[i]){
@@ -366,8 +367,7 @@ static int get_JPEG_info(u8* data, int data_size, int* out_w, int* out_h){
     }
     *out_w = w;
     *out_h = h;
-
-    return 0;
+    return i;
 }
 
 struct ya2d_texture* ya2d_load_JPEG_buffer(void* jpegbuf, unsigned long jpeg_size, int place){
@@ -381,29 +381,35 @@ struct ya2d_texture* ya2d_load_JPEG_buffer(void* jpegbuf, unsigned long jpeg_siz
     sceJpegInitMJpeg();
 
     get_JPEG_info(jpegbuf, jpeg_size, &w, &h);
-
-    bufRGB = malloc(4*w*h);
-    if (!bufRGB){
+    if (w <= 0 || h <= 0){
+        printf("bad image: %dx%d\n", w, h);
         goto ya2d_load_jpeg_error;
     }
 
-    if (w <= 0 || h <= 0 || (res=sceJpegCreateMJpeg(w, h))<0) {
-        goto ya2d_load_jpeg_error;
-    }
+    int w2 = next_pow2(w);
+    int h2 = next_pow2(h);
 
+    bufRGB = malloc(4*w2*h2);
     texture = ya2d_create_texture(w, h, GU_PSM_8888, place);
-    if (!texture){
+    if (!bufRGB || !texture){
+        printf("alloc FAILED: %p, %p\n", bufRGB, texture);
+        goto ya2d_load_jpeg_error;
+    }
+
+    if ((res=sceJpegCreateMJpeg(w2, h2))<0) {
+        printf("sceJpegCreateMJpeg FAILED: %p\n", res);
         goto ya2d_load_jpeg_error;
     }
 
     if ((res=sceJpegDecodeMJpeg(jpegbuf, jpeg_size, bufRGB, 0)) < 0){
+        printf("sceJpegCreateMJpeg FAILED: %p\n", res);
         goto ya2d_load_jpeg_error;
     }
 
     unsigned char* tex_data = texture->data;
     unsigned char* buf = bufRGB;
-    unsigned int wb = w*4;
-    for (int i=0; i<h; i++){
+    unsigned int wb = w2*4;
+    for (int i=0; i<h2; i++){
         memcpy(tex_data, buf, wb);
         buf += wb;
         tex_data += texture->stride;
