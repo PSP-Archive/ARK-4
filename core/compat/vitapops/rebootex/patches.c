7e80121d2b211c44e78cf1bdb950e2a94f6f73a2
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-09-12 03:12:07 +0200

    refactor

--- core/compat/vitapops/rebootex/patches.c
+++ core/compat/vitapops/rebootex/patches.c
@@ -6,8 +6,7 @@ int pspemuLfatOpenExtra(BootFile* file)
 {
     char* p = file->name;
     if (strcmp(p, "pspbtcnf.bin") == 0){
-        p[2] = 'v'; // custom btcnf for PS Vita
-        p[5] = 'x'; // psvbtxnf.bin for PS1 exploits
+        p[2] = 'x'; // custom btcnf for PS1
     }
     return -1;
 }
